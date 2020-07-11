499b9445b08c0621f465e06373cd68907184263a
Tomasz Sypko <tsypko@gmail.com>
2020-07-11 22:57:36 +0200

    undefined class in Section without modificator fixed

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./style.css";
 
 const Section = ({ title, body, classModificator }) => (
-    <section className={`section ${classModificator}`}>
+    <section className={`section${classModificator ? ` ${classModificator}` : ""}`}>
         <h2 className="section__header">{title}</h2>
         {body}
     </section>
