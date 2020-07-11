637a7bcb91d6000e4165d81216034a1d5da825cd
Tomasz Sypko <tsypko@gmail.com>
2020-07-11 21:59:49 +0200

    Updates

---
+++ precache-manifest.ae815cb20180782c021bd545cb08bb3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743c22c033d2e7a5053a758ada067b10",
+    "url": "/TASKER-To-Do-List-React/index.html"
+  },
+  {
+    "revision": "80c128392e8d1471d461",
+    "url": "/TASKER-To-Do-List-React/static/css/main.58e7ffc2.chunk.css"
+  },
+  {
+    "revision": "6fa2abe0bddf15594d7c",
+    "url": "/TASKER-To-Do-List-React/static/js/2.27e32e5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TASKER-To-Do-List-React/static/js/2.27e32e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c128392e8d1471d461",
+    "url": "/TASKER-To-Do-List-React/static/js/main.6606e47f.chunk.js"
+  },
+  {
+    "revision": "ed9d8f45e45b5662cc94",
+    "url": "/TASKER-To-Do-List-React/static/js/runtime-main.5c57046c.js"
+  },
+  {
+    "revision": "ad3ce474d04dd625db98b72960962ccb",
+    "url": "/TASKER-To-Do-List-React/static/media/checked.ad3ce474.svg"
+  },
+  {
+    "revision": "808985cb037b3e8d2715a2b425cf8e30",
+    "url": "/TASKER-To-Do-List-React/static/media/logo.808985cb.svg"
+  },
+  {
+    "revision": "04d13a08d2eb0dbc70fb00b7caee7b89",
+    "url": "/TASKER-To-Do-List-React/static/media/trash.04d13a08.svg"
+  }
+]);
