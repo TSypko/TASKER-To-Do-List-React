e449566e207df4f882b1d127507efbfadb46222a
Tomasz Sypko <tsypko@gmail.com>
2020-07-13 23:32:38 +0200

    App function refactored

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Switch from './Switch';
 import Header from './Header';
-import Logo from "./Logo";
 import Main from './Main';
 import Section from './Section';
 import Form from './Form';
 import Tasks from './Tasks';
 import Footer from './Footer';
-import Buttons from './Buttons';
-
 
 const tasks = [
   { id: 1, content: "Oglądnąć lekcje o React", done: true },
@@ -17,21 +14,17 @@ const tasks = [
 ];
 const hideDoneTasks = false;
 
-
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Switch />
-      <Header title="TASKER" body={<Logo altText="logotype" />}>
-      </Header>
+      <Header title="TASKER" />
       <Main>
-        <Section title="add new task" body={<Form placeHolderText={"write your task here"} />} />
-        <Section title="task list" body={<Tasks key={tasks} tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
+        <Section title="add new task" body={<Form />} />
+        <Section title="task list" body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
       </Main>
-      <Footer tasks={tasks}>
-        <Section classModificator={"section--footer"} body={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
-      </Footer>
-    </React.Fragment>
+      <Footer tasks={tasks} hideDoneTasks={hideDoneTasks}/>
+    </>
   );
 }
 
