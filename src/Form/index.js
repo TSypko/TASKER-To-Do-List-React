a2ed0980b50b67b8285b1a7a51837f6445586cc2
Tomasz Sypko <tsypko@gmail.com>
2020-07-13 23:10:56 +0200

    Form component refactored

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./style.css";
 
-const Form = ({ placeHolderText }) => (
+const Form = () => (
     <form className="form">
         <input
             type="text"
             className="form__input"
             autoFocus
-            placeholder={placeHolderText}
+            placeholder="write your task here"
         />
         <button className="form__button form__button--addButton">
             submit
