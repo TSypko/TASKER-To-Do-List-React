582f3948b692aff15118ae269a16c722db18f199
Tomasz Sypko <tsypko@gmail.com>
2020-07-13 23:16:34 +0200

    Section component refactored

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./style.css";
 
-const Section = ({ title, body, classModificator }) => (
-    <section className={`section${classModificator ? ` ${classModificator}` : ""}`}>
+const Section = ({ title, body, sectionType }) => (
+    <section className={`section${sectionType ? ` ${sectionType}` : ""}`}>
         <h2 className="section__header">{title}</h2>
         {body}
     </section>
