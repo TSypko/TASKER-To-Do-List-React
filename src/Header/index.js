ba4edf846186a2070cb649c1ad22d49c817e206c
Tomasz Sypko <tsypko@gmail.com>
2020-07-13 23:13:34 +0200

    Header component refactored

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./style.css";
+import Logo from "../Logo";
 
 const Header = ({ body, title }) => (
     <header className="header">
         <section className="header__section">
-            {body}
+           <Logo />
             <h1 className="header__title">{title}</h1>
         </section>
     </header>
