6ef463f4d34b6dcd521e46067829ae6a70ccfb65
Tomasz Sypko <tsypko@gmail.com>
2020-07-13 23:18:48 +0200

    Buttons component refactored

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-
-    tasks.length > 0 &&
-    (<React.Fragment>
-        <button className="button button--doneAllButton" disabled={tasks.every(({ done }) => done)}>set all as done</button>
-        <button className="button button--hideDoneButton" disabled={tasks.every(({ done }) => !done || undefined)}>
-            {hideDoneTasks ? "show" : "hide"} done</button>
-        <button className="button button--removeAllButton">remove all tasks</button>
-    </React.Fragment>)
+    tasks.length > 0 && (
+        <React.Fragment>
+            <button className="button button--doneAllButton"
+                disabled={tasks.every(({ done }) => done)}>set all as done</button>
+            <button className="button button--hideDoneButton"
+                disabled={tasks.every(({ done }) => !done || undefined)}>
+                {hideDoneTasks ? "show" : "hide"} done</button>
+            <button className="button button--removeAllButton">remove all tasks</button>
+        </React.Fragment>
+    )
 )
 
 export default Buttons;
