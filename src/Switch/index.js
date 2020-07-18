bf48262e1c65ac28dfa1f425f95da5936306fd12
Tomasz Sypko <tsypko@gmail.com>
2020-07-18 20:34:40 +0200

    night mode enabled

--- src/Switch/index.js
+++ src/Switch/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const Switch = () => (
-  <aside className="switch__container">
-    <label htmlFor="switch" className="switch__label">
-      Night Mode
+const Switch = () => {
+  const [theme, setTheme] = useState(false)
+  const onThemeChange = ({ target }) => setTheme(() => {
+    if (target.checked) {
+      document.body.setAttribute("data-theme", "night");
+    } else {
+      document.body.removeAttribute("data-theme");
+    }
+  }
+  );
+  return (
+    <aside className="switch__container">
+      <label htmlFor="switch" className="switch__label">
+        Night Mode
     </label>
-    <input className="switch" type="checkbox" id="switch" />
-    <label className="switch__toggle" htmlFor="switch"></label>
-  </aside>
-);
+      <input
+        className="switch"
+        type="checkbox"
+        id="switch"
+        value={theme}
+        onChange={onThemeChange}
+      />
+      <label className="switch__toggle" htmlFor="switch"></label>
+    </aside>
+  )
+};
 
 export default Switch;
