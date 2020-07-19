c7ee7452bde32b98d226a639f1586413a1c198f0
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 09:36:22 +0200

    Tasks component updated

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,24 +1,34 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="task__list">
-        {tasks.map((task) =>
-            <li key={task.id} className={`task__item ${
-                task.done && hideDone ? "task__item--hidden" : ""
-                }`}>
-                <button onClick={() => toggleTaskDone(task.id)} className={`task__button task__button--doneButton${
-                    task.done ? " task__button--doneButtonToggled" : ""
-                    }`}></button>
-                <p className={`task__paragraph ${
-                    task.done ? "task__paragraph--done" : ""
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => {
+    if (!tasks.length) {
+        return (
+            <p className="tasks__paragraph tasks__paragraph--centered">
+                There are currently no tasks to display
+            </p>
+        )
+    }
+    else return (
+        <ul className="tasks__list">
+            {tasks.map((task) =>
+                <li key={task.id} className={`tasks__item ${
+                    task.done && hideDone ? "tasks__item--hidden" : ""
                     }`}>
-                    {task.content}
-                </p>
-                <button onClick={() => removeTask(task.id)} className="task__button task__button--deleteButton"></button>
-            </li >
-        )}
-    </ul >
-);
+                    <button onClick={() => toggleTaskDone(task.id)}
+                        className={`tasks__button tasks__button--doneButton${
+                            task.done ? " tasks__button--doneButtonToggled" : ""
+                            }`}></button>
+                    <p className={`tasks__paragraph ${
+                        task.done ? "tasks__paragraph--done" : ""
+                        }`}>
+                        {task.content}
+                    </p>
+                    <button onClick={() => removeTask(task.id)} className="tasks__button tasks__button--deleteButton"></button>
+                </li >
+            )}
+        </ul >
+    )
+};
 
 export default Tasks;
