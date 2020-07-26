8e83967668b16a794ecdc41aa2fe2eeeef361ea2
Tomasz Sypko <tsypko@gmail.com>
2020-07-26 11:53:40 +0200

    props renamed

--- src/Switch/index.js
+++ src/Switch/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 
-const Switch = ({value, type, event}) => (
+const Switch = ({value, state, event}) => (
 
     <aside className="switch">
       <label htmlFor="switch" className="switch__label">
         Night Mode
     </label>
       <button
-        className={type}
+        className={state}
         id="switch"
         value={value}
         onClick={event}
