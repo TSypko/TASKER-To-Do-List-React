d18d139fc051a6fabcd816d236e219f89b12ca64
Tomasz Sypko <tsypko@gmail.com>
2020-07-26 11:34:20 +0200

    FlexContainer added

---
+++ src/FlexContainer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+const FlexContainer = ({ children }) => (
+    <div lassName=".flexContainer">
+        {children}
+    </div>
+
+);
+
+export default FlexContainer;
