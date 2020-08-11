010e02c03e7e99e4c99c94271a94277f0a6e858b
Tomasz Sypko <tsypko@gmail.com>
2020-08-11 23:14:06 +0200

    Logo refactored

--- src/App/Logo/index.js
+++ src/App/Logo/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Image } from "./styled"
-import logo from '../logo.svg';
+import logo from './logo.svg';
 
 const Logo = () => (
     <Image
         src={logo}
         alt="Logotype"
-        className="logo"
     />
 );
 
