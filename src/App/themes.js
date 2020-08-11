299bb6c345493a434b2abfe6740fa5f55cb29276
Tomasz Sypko <tsypko@gmail.com>
2020-08-11 09:02:34 +0200

    themes added

---
+++ src/App/themes.js
@@ -0,0 +1,26 @@
+
+  export const themeLight = {
+    background: "#c4bb91",
+    section_bg: "#fff",
+    section_text: "#141414",
+    shadow: "#555",
+    greenButton: "rgb(0, 172, 29)",
+    greenButton_lighter: "rgb(0, 218, 36)",
+    redButton: "rgb(172, 0, 0)",
+    redButton_lighter: "hsl(0, 100%, 45%)",
+    switch: "hsl(0, 0%, 100%)",
+    svg_image: "brightness(100%) invert(0%)",
+  };
+
+  export const themeDark = {
+    background: "#1a1a1a",
+    section_bg: "#2a3a41",
+    section_text: "rgb(255, 255, 255)",
+    shadow: "rgb(156, 156, 156)",
+    greenButton: "rgb(0, 172, 29)",
+    greenButton_lighter: "rgb(0, 218, 36)",
+    redButton: "rgb(172, 0, 0)",
+    redButton_lighter: "hsl(0, 100%, 45%)",
+    switch: "#2a3a41",
+    svg_image: "brightness(0%) invert(100%)",
+  };
