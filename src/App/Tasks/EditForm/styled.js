d2f2be4985a44d944887956d6e634dd089c6c621
Tomasz Sypko <tsypko@gmail.com>
2020-08-25 14:24:04 +0200

    style changed for button

--- src/App/Tasks/EditForm/styled.js
+++ src/App/Tasks/EditForm/styled.js
@@ -1,59 +1,50 @@
 import styled, { css } from "styled-components";
 
+export const FormContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        flex-wrap: wrap;
+    }
+`;
+
 export const EditFormInput = styled.input`
     width: 100%;
-    padding: 10px 20px; 
+    padding: 5px 20px; 
     display: none;
 
     ${({ edit }) => edit && css`
         display: unset;
     `}   
 `;
-export const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-`;
-    
+
 export const Button = styled.button`
     text-transform: uppercase;
     cursor: pointer;
     border: none;
-    padding: 10px 20px;
-    color: #ffffff;
-    font-weight: bold;
+    min-width: max-content;
+    background-color: ${({ theme }) => theme.colors.sectionText};
+    color: ${({ theme }) => theme.colors.sectionBackground};
     letter-spacing: 1px;
-    margin: 10px;
-    transition: color 0.4s;
-    transition: transform 0.1s;
-    transition: background-color 0.4s;
+    padding: 5px 10px;
+    margin-left: 10px;
+    transition: color 0.4s, transform 0.1s, background-color 0.4s;
 
-    ${({ submit }) => submit && css`
-        background-color: ${({ theme }) => theme.colors.greenButton};
-        border-radius: 5px;
-        transition: 0.5s;
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        margin-top: 10px;
+        margin-left: 0px;
+    }
 
-        &:hover {
-            filter: brightness(110%);
-        }
-        
-        &:active {
-            transform: scale(0.9);
-            filter: brightness(120%);
-        }
-    `}
-
-    ${({ cancel }) => cancel && css`
-            background-color: ${({ theme }) => theme.colors.redButton};
-            border-radius: 5px;
-            transition: 0.3s;
-
-        &:hover {
-            background-color: ${({ theme }) => theme.colors.redButtonLighter};
-        }
-        
-        &:active {
-            transform: scale(0.9);
-        }
-    `}
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.sectionBackground};
+        color: ${({ theme }) => theme.colors.sectionText};
+        border: 1px solid ${({ theme }) => theme.colors.sectionText};
+        font-weight: bold;
+        padding-left: 7px;
+    }
+    &:active {
+        border: 1px solid ${({ theme }) => theme.colors.sectionText};
+        transform: scale(0.9);
+    }
 `;
