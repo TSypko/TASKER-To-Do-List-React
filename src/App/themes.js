2237d3807aca527745a33a445a63c20b6e6fd4da
Tomasz Sypko <tsypko@gmail.com>
2020-08-25 11:01:31 +0200

    breakpoints added

--- src/App/themes.js
+++ src/App/themes.js
@@ -1,28 +1,41 @@
 export const themes = {
-    light:   {
-        background: "#c4bb91",
-        sectionBackground: "#fff",
-        sectionText: "#141414",
-        shadow: "#555",
-        greenButton: "rgb(0, 172, 29)",
-        greenButtonLighter: "rgb(0, 218, 36)",
-        redButton: "rgb(172, 0, 0)",
-        redButtonLighter: "hsl(0, 100%, 45%)",
-        switch: "hsl(0, 0%, 100%)",
-        svgImage: "brightness(100%) invert(0%)",
-        translate: "translatex(130%)",
+    light:
+    {
+        colors: {
+            background: "#c4bb91",
+            sectionBackground: "#fff",
+            sectionText: "#141414",
+            shadow: "#555",
+            greenButton: "rgb(0, 172, 29)",
+            redButton: "rgb(172, 0, 0)",
+            switch: "hsl(0, 0%, 100%)",
+            svgImage: "brightness(100%) invert(0%)",
+            translate: "translatex(130%)",
         },
-    dark:   {
-        background: "#1a1a1a",
-        sectionBackground: "#2a3a41",
-        sectionText: "rgb(255, 255, 255)",
-        shadow: "rgb(156, 156, 156)",
-        greenButton: "rgb(0, 172, 29)",
-        greenButtonLighter: "rgb(0, 218, 36)",
-        redButton: "rgb(172, 0, 0)",
-        redButtonLighter: "hsl(0, 100%, 45%)",
-        switch: "#2a3a41",
-        svgImage: "brightness(0%) invert(100%)",
-        translate: "translatex(0%)",
-        }
+        breakpoints: {
+            phone: "375px",
+            tablet: "768px",
+            desktop: "1200px",
+
+        },
+    },
+    dark:
+    {
+        colors: {
+            background: "#1a1a1a",
+            sectionBackground: "#2a3a41",
+            sectionText: "rgb(255, 255, 255)",
+            shadow: "rgb(156, 156, 156)",
+            greenButton: "rgb(0, 172, 29)",
+            redButton: "rgb(172, 0, 0)",
+            switch: "#2a3a41",
+            svgImage: "brightness(0%) invert(100%)",
+            translate: "translatex(0%)",
+        },
+        breakpoints: {
+            phone: "375px",
+            tablet: "768px",
+            desktop: "1200px",
+        },
+    },
 };
