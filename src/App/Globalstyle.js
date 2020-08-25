d1ec91b766641812a82e8caa02f25c6ff5da4833
Tomasz Sypko <tsypko@gmail.com>
2020-08-25 11:01:23 +0200

    added media queries

--- src/App/Globalstyle.js
+++ src/App/Globalstyle.js
@@ -7,8 +7,13 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: inherit;
   }
   body {
-    background: ${({ theme }) => theme.background};
+    background: ${({ theme }) => theme.colors.background};
     font-family: "Roboto Condensed", sans-serif;
     transition: all 0.3s linear;
+    margin: 10px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.phone}) {
+        font-size: 15px;
+    }
   }
   `
