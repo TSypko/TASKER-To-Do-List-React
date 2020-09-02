5fb12787120181c056bb2e0f7703699189141703
Tomasz Sypko <tsypko@gmail.com>
2020-09-02 17:05:18 +0200

    theme feature refactored to Redux usage

--- src/App/features/theme/Switch/index.js
+++ src/App/features/theme/Switch/index.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { themeChange, selectTheme } from "../themeSlice";
 import { SwitchContainer, Label, Button } from "./styled"
 
-const Switch = ({ onChange }) => (
+const Switch = () => {
 
-  <SwitchContainer>
-    <Label htmlFor="switch">
-      Night Mode
-    </Label>
-    <Button
-      id="switch"
-      onClick={onChange}
-    />
-    <Label toggle htmlFor="switch"></Label>
-  </SwitchContainer>
-  
-);
+  const { theme } = useSelector(selectTheme);
+  const dispatch = useDispatch()
+
+  return (
+
+    <SwitchContainer>
+      <Label htmlFor="switch">
+        Night Mode
+       </Label>
+      <Button
+        id="switch"
+        onClick={() => dispatch(themeChange(theme))}
+      />
+      <Label toggle htmlFor="switch"></Label>
+    </SwitchContainer>
+
+  )
+};
 
 export default Switch;
--- src/App/features/theme/Theme.js
+++ src/App/features/theme/Theme.js
@@ -2,32 +2,22 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./themes";
 import { GlobalStyles } from "./Globalstyle";
-import { useLocalStorageState } from "../../useLocalStorageState";
+import { useSelector } from "react-redux";
+import { selectTheme } from "./themeSlice"
 import Wrapper from '../../common/Wrapper';
 import FlexContainer from '../../common/FlexContainer';
 import Switch from "./Switch";
 
 function Theme({ body }) {
 
-    const [theme, setTheme] = useLocalStorageState("currentTheme", "light");
-
-    const themeChange = () => {
-        if (theme === "light") {
-            setTheme("dark")
-        }
-        else {
-            setTheme("light")
-        };
-    };
+    const { theme } = useSelector(selectTheme);
 
     return (
         <ThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
             <GlobalStyles />
             <Wrapper>
                 <FlexContainer>
-                    <Switch
-                        onChange={themeChange}
-                    />
+                    <Switch />
                     {body}
                 </FlexContainer>
             </Wrapper>
