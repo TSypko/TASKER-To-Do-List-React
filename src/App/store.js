5d908768c3f3f742ed986311150beea4f09d3c59
Tomasz Sypko <tsypko@gmail.com>
2020-09-02 16:45:28 +0200

    reducer added

--- src/App/store.js
+++ src/App/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from "./features/tasks/tasksSlice"
+import tasksReducer from "./features/tasks/tasksSlice";
+import hideDoneReducer from "./features/tasks/hideDoneSlice";
 
 export default configureStore({
     reducer: {
         tasks: tasksReducer,
+        hideDone: hideDoneReducer,
     },
 });
