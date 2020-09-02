86c7dff9c8eba199d0117141f8a0827a6fce104c
Tomasz Sypko <tsypko@gmail.com>
2020-09-02 14:01:13 +0200

    added Provder with store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from "./App/store"
 import App from './App/App';
 import * as serviceWorker from './App/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
