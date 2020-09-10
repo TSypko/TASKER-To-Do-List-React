6bb78a6bf051a124e77d2fc7b51a2c630c4a57fe
Tomasz Sypko <tsypko@gmail.com>
2020-09-10 13:06:37 +0200

    App structure changed

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Tasks from "./features/tasks/Tasks";
 import Theme from "./features/theme/Theme";
+import { useSelector } from "react-redux";
+import { selectTheme } from "./features/theme/themeSlice";
+import { GlobalStyles } from "./features/theme/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./../App/features/theme/themes";
+import Wrapper from './common/Wrapper';
+import FlexContainer from './common/FlexContainer';
+
 
 function App() {
 
+  const { theme } = useSelector(selectTheme);
+
   return (
-    <Theme body={<Tasks />} />
+      <ThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
+        <GlobalStyles />
+        <Wrapper>
+          <FlexContainer>
+            <Theme />
+            <Tasks />
+          </FlexContainer>
+        </Wrapper>
+      </ThemeProvider>
   );
 };
 
--- src/App/features/theme/Theme.js
+++ src/App/features/theme/Theme.js
@@ -1,33 +1,9 @@
-import React, { useEffect } from "react";
-import { ThemeProvider } from "styled-components";
-import { themes } from "./themes";
-import { GlobalStyles } from "./Globalstyle";
-import { useSelector } from "react-redux";
-import { selectTheme } from "./themeSlice"
-import Wrapper from '../../common/Wrapper';
-import FlexContainer from '../../common/FlexContainer';
+import React from "react";
 import Switch from "./Switch";
 
-function Theme({ body }) {
+const Theme = () => (
+    <Switch />
+);
 
-    const { theme } = useSelector(selectTheme);
-
-    useEffect(() => {
-        localStorage.setItem("currentTheme",
-            JSON.stringify(theme));
-    }, [theme]);
-
-    return (
-        <ThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
-            <GlobalStyles />
-            <Wrapper>
-                <FlexContainer>
-                    <Switch />
-                    {body}
-                </FlexContainer>
-            </Wrapper>
-        </ThemeProvider>
-    );
-};
 
 export default Theme;
