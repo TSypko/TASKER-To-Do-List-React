09a22b360b6ccb9c9a3b0e971c60f6315f23e340
Tomasz Sypko <tsypko@gmail.com>
2020-09-10 10:09:06 +0200

    store with subscribes added

--- src/App/store.js
+++ src/App/store.js
@@ -2,9 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasksSlice";
 import themeReducer from "./features/theme/themeSlice";
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         tasks: tasksReducer,
         theme: themeReducer,
     },
 });
+
+store.subscribe(() => {
+    localStorage.setItem("currentTheme", JSON.stringify(store.getState().theme.theme))
+    localStorage.setItem("tasks", JSON.stringify(store.getState().tasks.tasks))
+    localStorage.setItem("hideDone", JSON.stringify(store.getState().tasks.hideDone))
+})
+
+export default store;
