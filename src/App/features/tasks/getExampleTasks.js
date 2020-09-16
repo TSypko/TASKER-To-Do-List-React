fd5d7e2d2d8bdbf3ce4977731eab4f8513568182
Tomasz Sypko <tsypko@gmail.com>
2020-09-16 23:13:51 +0200

    bug fixed

--- src/App/features/tasks/getExampleTasks.js
+++ src/App/features/tasks/getExampleTasks.js
@@ -1,10 +1,10 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/TASKER-To-Do-List-React/exampleTasks.js");
+    const response = await fetch("/TASKER-To-Do-List-React/exampleTasks.json");
 
     if (!response.ok) {
         new Error(response.statusText);
     }
-    
+
     return await response.json();
 
 };
