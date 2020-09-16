db95382b094040388e52bef638be18dedc4203bb
Tomasz Sypko <tsypko@gmail.com>
2020-09-17 00:58:26 +0200

    rootSaga moved

--- src/App/features/tasks/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchExampleTasks } from "./tasksSaga";
-
-export default function* rootSaga() {
-    yield all([
-        watchFetchExampleTasks(),
-    ]);
-}
