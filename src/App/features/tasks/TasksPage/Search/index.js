5dac7e749f2cf52ade30fbb08738f5abdd1e2b63
Tomasz Sypko <tsypko@gmail.com>
2020-09-17 23:19:35 +0200

    Search component created

---
+++ src/App/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormInput from '../FormInput'
+
+const Search = () => {
+    return (
+        <div>
+            <FormInput  placeholder="search for task here"/>
+        </div>
+    )
+}
+
+export default Search
