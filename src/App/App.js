3a95eccb777410006a3a3cb1151a84045e63058d
Tomasz Sypko <tsypko@gmail.com>
2020-09-17 20:59:42 +0200

    Added Routing

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Tasks from "./features/tasks/Tasks";
 import Theme from "./features/theme/Theme";
 import { useSelector } from "react-redux";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { selectTheme } from "./features/theme/themeSlice";
 import { GlobalStyles } from "./features/theme/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./../App/features/theme/themes";
 import Wrapper from './common/Wrapper';
 import FlexContainer from './common/FlexContainer';
+import Menu from "./common/Menu";
+import Author from "./features/author/Author";
 
 
 function App() {
@@ -15,15 +18,25 @@ function App() {
   const { theme } = useSelector(selectTheme);
 
   return (
-      <ThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
-        <GlobalStyles />
+    <ThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
+      <GlobalStyles />
+      <HashRouter>
         <Wrapper>
           <FlexContainer>
             <Theme />
-            <Tasks />
+            <Menu />
+            <Switch>
+              <Route path="/author">
+                <Author />
+              </Route>
+              <Route path="/">
+                <Tasks />
+              </Route>
+            </Switch>
           </FlexContainer>
         </Wrapper>
-      </ThemeProvider>
+      </HashRouter>
+    </ThemeProvider>
   );
 };
 
