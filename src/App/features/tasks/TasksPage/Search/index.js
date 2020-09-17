54ba04d3efaceeba551b74ea7b3e367e18d7e947
Tomasz Sypko <tsypko@gmail.com>
2020-09-18 00:40:46 +0200

    added search possibility by query

--- src/App/features/tasks/TasksPage/Search/index.js
+++ src/App/features/tasks/TasksPage/Search/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import FormInput from '../FormInput'
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import FormInput from '../FormInput';
+import searchQueryParamName from "../../searchQueryParamName";
 
 const Search = () => {
+    const location = useLocation();
+    const history = useHistory();
+    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target.value.trim() === "")
+            searchParams.delete(searchQueryParamName);
+        else {
+            searchParams.set(searchQueryParamName, target.value)
+        }
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+
     return (
         <div>
-            <FormInput  placeholder="search for task here"/>
+            <FormInput
+                placeholder="search for task here"
+                value={query || ""}
+                onChange={onInputChange}
+            />
         </div>
-    )
-}
+    );
+};
 
-export default Search
+export default Search;
