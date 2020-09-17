9646d043bc0c85e8e7992609db79f4269f717708
Tomasz Sypko <tsypko@gmail.com>
2020-09-17 20:59:11 +0200

    provisional Author component created

---
+++ src/App/features/author/Author.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Main from '../../common/Main';
+import Section from '../../common/Section';
+
+function Author() {
+
+    return (
+        <>
+            <Main>
+                <Section
+                    title="About"
+                    body={
+                        <>o aplikacji</>
+                    }
+                />
+            </Main>
+        </>
+    );
+};
+
+export default Author;
