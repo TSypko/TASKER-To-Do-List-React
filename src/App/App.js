fa97558628c84fc36277ad9f49c33b819c078519
Tomasz Sypko <tsypko@gmail.com>
2020-09-18 09:10:59 +0200

    redirect added

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import Theme from "./features/theme/Theme";
 import { useSelector } from "react-redux";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { selectTheme } from "./features/theme/themeSlice";
 import { GlobalStyles } from "./features/theme/GlobalStyles";
 import { ThemeProvider } from "styled-components";
@@ -11,7 +11,7 @@ import { themes } from "./../App/features/theme/themes";
 import Wrapper from './common/Wrapper';
 import FlexContainer from './common/FlexContainer';
 import Menu from "./common/Menu";
-import Author from "./features/author/AboutPage";
+import AboutPage from "./features/about/AboutPage";
 
 function App() {
 
@@ -34,8 +34,11 @@ function App() {
               <Route path="/tasks">
                 <TasksPage />
               </Route>
-              <Route path="/author">
-                <Author />
+              <Route path="/about">
+                <AboutPage />
+              </Route>
+              <Route path="/">
+                <Redirect to="/tasks" />
               </Route>
             </Switch>
           </FlexContainer>
